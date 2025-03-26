afa0ffe458cad2851c8526900f8368cdc78d84b3
kavinrog <kavinderroghit.k@gmail.com>
2025-03-26 12:52:32 -0700

    Fix Typo and Add testing

--- Graphs/surroundedregion_LC130.py
+++ Graphs/surroundedregion_LC130.py
@@ -11,15 +11,15 @@ def solve(board):
             dfs(r+dr, c+dc)
         
     for r in range(ROWS):
-        if board[r][0] == 0:
+        if board[r][0] == "O":
             dfs(r, 0)
-        if board[r][COLS-1] == 0:
+        if board[r][COLS-1] == "O":
             dfs(r,COLS-1)
     
     for c in range(COLS):
-        if board[0][c] == 0:
+        if board[0][c] == "O":
             dfs(0, c)
-        if board[ROWS-1][c] == 0:
+        if board[ROWS-1][c] == "O":
             dfs(ROWS-1,c)
             
     for r in range(ROWS):
@@ -29,3 +29,14 @@ def solve(board):
             elif board[r][c] == "T":
                 board[r][c] = "O"        
     
+board = [
+    ["X", "X", "X", "X"],
+    ["X", "O", "O", "X"],
+    ["X", "X", "O", "X"],
+    ["X", "O", "X", "X"]
+]
+
+solve(board)
+
+for row in board:
+    print(row)
