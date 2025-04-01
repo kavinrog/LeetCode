840134b4b50f236392cdc1e95c80371007f6873c
kavinrog <kavinderroghit.k@gmail.com>
2025-04-01 14:17:19 -0700

    Fix Bug and add test code

--- Intervals/Meeting Room II.py
+++ Intervals/Meeting Room II.py
@@ -1,15 +1,17 @@
 def minmeetingroom(intervals):
-    start = sorted([i.start for i in intervals])
-    end = sorted([i.end for i in intervals])
+    start = sorted([i[0] for i in intervals])
+    end = sorted([i[1] for i in intervals])
     res = count = 0 
     s = e = 0 
     while s < len(intervals):
         if start[s] < end[e]:
-            count+=1
             s+=1
+            count+=1      
         else:
             e+=1
             count-=1
         res = max(count, res)
     return res 
-    
+
+intervals=[(0,40),(5,10),(15,20)]
+print(minmeetingroom(intervals=intervals))
