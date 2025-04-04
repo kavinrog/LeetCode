ee01f461d5369e5177a063089a97cd42e00a6992
kavinrog <kavinderroghit.k@gmail.com>
2025-04-03 20:34:21 -0700

    Add code for solving the problem of pacific and atlantic

---
+++ Graphs/PacificAtlantic.py
@@ -0,0 +1,28 @@
+def pacificAtlantic(heights):
+    if not heights and not heights[0]:
+        return []
+    ROWS, COLS = len(heights), len(heights[0])
+    pacific = set()
+    atlantic = set()
+    directions = [(1,0),(-1,0),(0,1),(0,-1)]
+    
+    def dfs(r, c ,visited, prevHeight):
+        if (r < 0 or r >= ROWS or 
+            c < 0 or c >= COLS or 
+            (r, c) in visited or
+            heights[r][c]<prevHeight):
+            return
+        visited.add((r,c))
+        for dr, dc in directions:
+            dfs(r, c, visited, heights[r][c])
+    
+    for c in range(COLS):
+        dfs(0, c, pacific, heights[0][c])
+        dfs(ROWS-1, c, atlantic, heights[ROWS-1][c])
+    
+    for r in range(ROWS):
+        dfs(r, 0, pacific, heights[r][0])
+        dfs(r, COLS-1, atlantic, heights[r][COLS-1]])
+    return list(pacific & atlantic)
+        
+        
