585a3e3d00ed02573067929c0f0dc59e1489bf33
kavinrog <kavinderroghit.k@gmail.com>
2025-04-26 21:16:28 -0700

    Add Test Function

--- Heaps/ksmallestsortedarray.py
+++ Heaps/ksmallestsortedarray.py
@@ -15,3 +15,16 @@ def ksmallestsortedarray(arrays, k):
             heapq.heappush(heap, (arrays[arr_idx][ele_idx +1],arr_idx, ele_idx +1))
     return -1 
 
+# Test the function
+if __name__ == "__main__":
+    arrays = [[1, 5, 9], [2, 6, 10], [3, 7, 11]]
+    k = 5
+    print(ksmallestsortedarray(arrays, k))  # Output: 5
+    
+    arrays = [[1, 2], [3, 4], [5, 6]]
+    k = 4
+    print(ksmallestsortedarray(arrays, k))  # Output: 4
+    
+    arrays = [[1], [2], [3]]
+    k = 2
+    print(ksmallestsortedarray(arrays, k))  # Output: 2
