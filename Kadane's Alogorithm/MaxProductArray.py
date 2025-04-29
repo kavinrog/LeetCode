44bd4effef07e9972e4db35e2c9848a87e44be74
kavinrog <kavinderroghit.k@gmail.com>
2025-04-28 22:30:01 -0700

    Add function to test it

--- Kadane's Alogorithm/MaxProductArray.py
+++ Kadane's Alogorithm/MaxProductArray.py
@@ -10,3 +10,11 @@ def maxProductArray(arr):
         global_max = max(global_max, curr_max)
     return global_max
         
+# Test the function
+if __name__ == "__main__":
+    arr = [2, 3, -2, 4]
+    print(maxProductArray(arr))  # Output: 6
+    arr = [-2, 0, -1]
+    print(maxProductArray(arr))  # Output: 0
+    arr = [-2, 3, -4]
+    print(maxProductArray(arr))  # Output: 24
